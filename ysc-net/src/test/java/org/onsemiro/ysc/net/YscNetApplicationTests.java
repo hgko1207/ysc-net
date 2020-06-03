6b9e0c2bf82b4fc8690830b6149d38fbdd49e275
hgko <hgko@inspace.re.kr>
2020-06-03 15:30:22 +0900

    first commit

---
+++ ysc-net/src/test/java/org/onsemiro/ysc/net/YscNetApplicationTests.java
@@ -0,0 +1,13 @@
+package org.onsemiro.ysc.net;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YscNetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
