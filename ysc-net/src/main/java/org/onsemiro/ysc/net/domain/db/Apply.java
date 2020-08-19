a881860d7a18ac983471e8cd2f91f19683da33c8
hgko1207 <khkkhk1207@gmail.com>
2020-08-19 09:31:41 +0900

    수정

---
+++ ysc-net/src/main/java/org/onsemiro/ysc/net/domain/db/Apply.java
@@ -0,0 +1,16 @@
+package org.onsemiro.ysc.net.domain.db;
+
+import org.onsemiro.ysc.net.domain.Domain;
+
+import lombok.Data;
+
+/**
+ * 신청합니다 관리 테이블 도메인
+ * 
+ * @author hgko
+ *
+ */
+@Data
+public class Apply implements Domain {
+
+}
